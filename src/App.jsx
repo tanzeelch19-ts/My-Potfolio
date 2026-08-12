63dab4622aaedcdb233054b4d0f0c7bfb3ec3eff
Tanzeel Ch <tanzeelch19@gmail.com>
2026-08-11 23:36:25 -0700

    Fix GitStats component name mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ export default function App() {
         <Projects />
         <Education />
         <Experience />
-        <GithubStats />
+        <GitStats />
         <Contact />
         <Footer />
         <ScrollTop />
